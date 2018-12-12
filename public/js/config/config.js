e3833de22876b71ba26b820d24080d86621066ab
brianbentancourt <brianbentancourt9@gmail.com>
2018-12-12 15:03:09 -0300

    inicializando configuracion

---
+++ public/js/config/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCkuGFiO75DdmNbVl38J8EjqC2TDTpkf7U",
+    authDomain: "blogplatzi-c1c10.firebaseapp.com",
+    databaseURL: "https://blogplatzi-c1c10.firebaseio.com",
+    projectId: "blogplatzi-c1c10",
+    storageBucket: "blogplatzi-c1c10.appspot.com",
+    messagingSenderId: "94461732256"
+  };
